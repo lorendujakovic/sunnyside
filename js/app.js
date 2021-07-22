7c5d49940c87a9e10135c8e927243928cde9eac1
Loren Dujakovic <loren.dujakovic@mail.ch>
2021-07-22 21:30:02 +0200

    build header and navigation

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const menu = document.querySelector('.header__menu');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', () => {
+    if (header.classList.contains('open')) { //Close Hamburger Element
+        header.classList.remove('open');
+
+        fadeElems.forEach(element => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+        
+    }else {
+        header.classList.add('open');
+
+        fadeElems.forEach(element => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        
+
+    }
+});
